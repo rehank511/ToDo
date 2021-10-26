2dd273f8b6bc2654c9cb42075c9dcb2b707ada9a
rehank511 <ritik.kedia99@gmail.com>
2021-10-25 17:04:11 -0700

    added databse support

--- go.mod
+++ go.mod
@@ -2,9 +2,7 @@ module main
 
 go 1.17
 
-require github.com/jinzhu/gorm v1.9.16
-
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.9
 )
--- main.go
+++ main.go
@@ -1,50 +1,78 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-var Connector *gorm.DB                                                                                                                                                                                                                                                                                                                 
-
 type Todo struct {
-	ID string `json:"ID"`
-	Text string `json:"Description"`
-	CreateTime time.Time `json:"CreationTime"`
+	ID int64 `json:"ID"`
+	Text string `json:"Text"`
+	CreateTime time.Time `json:"Time"`
 }
 
-var todos []Todo
+var counter int64
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	db, err := sql.Open("sqlite3", "./tododata.db")
+	checkErr(err)
+	defer db.Close()
+	var todos []*Todo
+	rows, err := db.Query("SELECT * FROM Todos")
+	checkErr(err)
+	for rows.Next() {
+		t := new(Todo)
+		fmt.Print(rows.Scan())
+		err := rows.Scan(&t.ID, &t.Text, &t.CreateTime)
+		checkErr(err)
+		todos = append(todos, t)
+	}
 	json.NewEncoder(w).Encode(todos)
 }
 
 func addTodo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var todo Todo
-	_ = json.NewDecoder(r.Body).Decode(&todo)
-	todo.ID = strconv.Itoa(len(todos) + 1)
-	todos = append(todos, todo)
-	json.NewEncoder(w).Encode(todo)
+	db, err := sql.Open("sqlite3", "./tododata.db")
+	checkErr(err)
+	var todos []*Todo
+	params := mux.Vars(r)
+	counter += 1
+	stmt, err := db.Prepare("INSERT INTO Todos(ID, Text, CreateTime) VALUES(?,?,?)")
+	checkErr(err)
+	_ , err = stmt.Exec(counter, params["text"], time.Now())
+	checkErr(err)
+
+
+	t := new(Todo)
+	t.ID = counter
+	t.Text = params["text"]
+	t.CreateTime = time.Now()
+	todos = append(todos, t)
+	db.Close()
+
+	json.NewEncoder(w).Encode(todos)
 }
 
 func deleteTodo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	db, err := sql.Open("sqlite3", "./tododata.db")
+	checkErr(err)
 	params := mux.Vars(r)
-	for index, item := range todos {
-		if item.ID == params["id"] {
-			todos = append(todos[:index], todos[index+1:]...)
-			break
-		}
-	}
+
+	stmt, err := db.Prepare("DELETE FROM Todos WHERE ID=?")
+	checkErr(err)
+	_ , err = stmt.Exec(params["id"])
+	checkErr(err)
+	db.Close()
 }
 
 func updateTodo(w http.ResponseWriter, r *http.Request) {
@@ -52,25 +80,52 @@ func updateTodo(w http.ResponseWriter, r *http.Request) {
 	// delete that todo from list
 	// get the updaetd text
 	// make a new todo with new text
-	
+	db, err := sql.Open("sqlite3", "./tododata.db")
+	checkErr(err)
+	params := mux.Vars(r)
+
+	stmt, err := db.Prepare("update Todos set Text=? where ID=?")
+    checkErr(err)
+	_ , err = stmt.Exec(params["text"], params["id"])
+    checkErr(err)
+	db.Close()
+
+}
+
+func createTable(db *sql.DB) {
+	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS Todos (ID INT AUTO_INCREMENT(1,1), Text varchar(255), CreateTime DATETIME, PRIMARY KEY (ID))")
+	checkErr(err)
+	statement.Exec()
+	statement.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() { 
-	router := mux.NewRouter()
+	var _, err = os.Stat("./tododata.db")
 
-	layout := "01-02-2006 15:04:05"
-	timenow := time.Now().Format(layout)
-	now, error := time.Parse(layout, timenow)
-	if error != nil {
-		fmt.Println(error)
+	if os.IsNotExist(err) {
+		log.Println("Creating Database File...")
+		file, err := os.Create("tododata.db") // Create SQLite file
+		checkErr(err)
+		file.Close()
 	}
 
-	todos = append(todos, Todo{ID: "1", Text: "Get Groceries", CreateTime: now})
+	db, err := sql.Open("sqlite3", "./tododata.db")
+	checkErr(err)
+	createTable(db)
+	db.Close()
+
+	router := mux.NewRouter()
 
 	router.HandleFunc("/todo", homePage).Methods("GET")
-	router.HandleFunc("/todo", addTodo).Methods("POST")
+	router.HandleFunc("/todo/{text}", addTodo).Methods("POST")
 	router.HandleFunc("/todo/{id}", deleteTodo).Methods("DELETE")
-	router.HandleFunc("todo/{id}", updateTodo).Methods("PUT")
+	router.HandleFunc("todo/{id}, {text}", updateTodo).Methods("PUT")
 	
 	fmt.Println("Starting New Server")
 	log.Fatal(http.ListenAndServe(":3000", router))
